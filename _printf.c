f3d4eba73464c9ed6712b8e47177589509759472
Marvellous Olarinde <85411943+realmarvellomj@users.noreply.github.com>
2023-07-24 08:47:50 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,11 +3,13 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+*/
+
 int _printf(const char *format, ...)
+
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -53,11 +55,13 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
+
 void print_buffer(char buffer[], int *buff_ind)
+
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
