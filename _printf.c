f8a0d23719807fb49ff9eaa52d3b1a278d748c30
Marvellous Olarinde <85411943+realmarvellomj@users.noreply.github.com>
2023-07-25 13:49:40 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -23,13 +23,13 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+	if (format[i] != '%')
+	{
+		buffer[buff_ind++] = format[i];
+		if (buff_ind == BUFF_SIZE)
+			print_buffer(buffer, &buff_ind);
+		/* write(1, &format[i], 1);*/
+		printed_chars++;
 		}
 		else
 		{
